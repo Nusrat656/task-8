d1e27e8fac4bfc1f045fc3ccedfcefb83b1b8f2a
Nusrat656 <161738911+Nusrat656@users.noreply.github.com>
2024-04-02 16:22:39 +0400

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Employee
+    {
+        private int employeeID;
+        private string name;
+        private string position;
+        private double salary;
+
+        public int EmployeeID
+        {
+            get { return employeeID; }
+            private set { employeeID = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("NULL");
+                }
+            }
+        }
+
+        public string Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public double Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value >= 0)
+                {
+                    salary = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Error");
+                }
+            }
+        }
+
+        public Employee(int employeeID, string name, string position, double salary)
+        {
+            EmployeeID = employeeID;
+            Name = name;
+            Position = position;
+            Salary = salary;
+        }
+
+        public void UpdateSalary(double newSalary)
+        {
+            Salary = newSalary;
+        }
+
+        public void ChangePosition(string newPosition)
+        {
+            Position = newPosition;
+        }
+
+        public void DisplayEmployeeInfo()
+        {
+            Console.WriteLine($"Employee ID: {EmployeeID}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Position: {Position}");
+            Console.WriteLine($"Salary: {Salary}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Book myBook = new Book("Sonuncu Quyu", "Memmed Namazov", "NMC MMC", 205);
+            //Console.WriteLine("Book Information:");
+            //myBook.DisplayBookInfo();            
+            //myBook.IncreaseCopies(6); 
+            //myBook.DecreaseCopies(3); 
+            //Console.WriteLine("\nUpdated Book Information:");
+            //myBook.DisplayBookInfo();
+
+            //Employee emp1 = new Employee(101, "Memmed Namazov", "Sis Admin", 3000);
+            //Employee emp2 = new Employee(102, "Emin Ekberov", "Senyor", 2500);
+            //emp1.Name = "Memmed Namazov";
+            //emp2.UpdateSalary(3000);
+            //emp1.DisplayEmployeeInfo();
+            //Console.WriteLine();
+            //emp2.DisplayEmployeeInfo();
+        }
+    }
+    }
